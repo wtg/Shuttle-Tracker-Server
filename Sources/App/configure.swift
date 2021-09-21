0f9ad3d3c55cb7c92b599b25980c87dcd32dedaf
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2021-09-20 22:51:56 -0400

    Fix TLS configuration for Dokku

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -29,7 +29,22 @@ public func configure(_ application: Application) throws {
 		.wait()
 	try application.queues.startInProcessJobs()
 	try application.queues.startScheduledJobs()
-	if let domain = ProcessInfo.processInfo.environment["domain"] {
+	if FileManager.default.fileExists(atPath: "tls") {
+		print("TLS directory detected!")
+		try application.http.server.configuration.tlsConfiguration = .forServer(
+			certificateChain: [
+				.certificate(
+					NIOSSLCertificate(
+						file: "\(FileManager.default.currentDirectoryPath)/tls/server.crt",
+						format: .pem
+					)
+				)
+			],
+			privateKey: .file(
+				"\(FileManager.default.currentDirectoryPath)/tls/server.key"
+			)
+		)
+	} else if let domain = ProcessInfo.processInfo.environment["domain"] {
 		try application.http.server.configuration.tlsConfiguration = .forServer(
 			certificateChain: [
 				.certificate(
--- Sources/Runner/main.swift
+++ Sources/Runner/main.swift
@@ -8,7 +8,6 @@
 import Vapor
 import App
 
-print("Working directory: \(FileManager.default.currentDirectoryPath)")
 var environment = try Environment.detect()
 try LoggingSystem.bootstrap(from: &environment)
 let app = Application(environment)
