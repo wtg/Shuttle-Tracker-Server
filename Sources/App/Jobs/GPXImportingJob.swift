cd720bbab1b0ac2e4bba4cdae1585687dfe7b48c
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2021-09-21 14:23:31 -0400

    Reenable GPXImportingJob

--- Sources/App/Jobs/GPXImportingJob.swift
+++ Sources/App/Jobs/GPXImportingJob.swift
@@ -13,31 +13,33 @@ import CoreGPX
 struct GPXImportingJob: ScheduledJob {
 	
 	func run(context: QueueContext) -> EventLoopFuture<Void> {
-//		let bundle = Bundle(path: "/app/.swift-bin/Shuttle Tracker Server_App.resources")!
-//		let routeFileURL = bundle.url(forResource: "Route", withExtension: "gpx")!
-//		let gpx = GPXParser(withURL: routeFileURL)!.parsedData()!
-//		Route.query(on: context.application.db)
-//			.all()
-//			.mapEach { (route) in
-//				route.delete(on: context.application.db)
-//			}
-//			.whenSuccess { (_) in
-//				_ = Route(from: gpx.routes.first!).save(on: context.application.db)
-//			}
-//		Stop.query(on: context.application.db)
-//			.all()
-//			.mapEach { (stop) in
-//				stop.delete(on: context.application.db)
-//			}
-//			.whenSuccess { (_) in
-//				gpx.waypoints
-//					.map { (gpxWaypoint) in
-//						return Stop(from: gpxWaypoint)!
-//					}
-//					.forEach { (stop) in
-//						_ = stop.save(on: context.application.db)
-//					}
-//			}
+		let routeFileURL = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
+			.appendingPathComponent("Public", isDirectory: true)
+			.appendingPathComponent("route.gpx", isDirectory: false)
+		if let gpx = GPXParser(withURL: routeFileURL)?.parsedData() {
+			Route.query(on: context.application.db)
+				.all()
+				.mapEach { (route) in
+					route.delete(on: context.application.db)
+				}
+				.whenSuccess { (_) in
+					_ = Route(from: gpx.routes.first!).save(on: context.application.db)
+				}
+			Stop.query(on: context.application.db)
+				.all()
+				.mapEach { (stop) in
+					stop.delete(on: context.application.db)
+				}
+				.whenSuccess { (_) in
+					gpx.waypoints
+						.map { (gpxWaypoint) in
+							return Stop(from: gpxWaypoint)!
+						}
+						.forEach { (stop) in
+							_ = stop.save(on: context.application.db)
+						}
+				}
+		}
 		return context.eventLoop.future()
 	}
 	
