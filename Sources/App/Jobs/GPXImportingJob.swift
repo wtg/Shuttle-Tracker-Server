1ffb741075df6984e7c5feb19373b08f56b14198
Gabriel Jacoby-Cooper <gerzersoftware@icloud.com>
2021-09-20 20:33:14 -0400

    Fix Dokku deploy issues

--- Sources/App/Jobs/GPXImportingJob.swift
+++ Sources/App/Jobs/GPXImportingJob.swift
@@ -13,30 +13,31 @@ import CoreGPX
 struct GPXImportingJob: ScheduledJob {
 	
 	func run(context: QueueContext) -> EventLoopFuture<Void> {
-		let routeFileURL = Bundle.module.url(forResource: "Route", withExtension: "gpx")!
-		let gpx = GPXParser(withURL: routeFileURL)!.parsedData()!
-		Route.query(on: context.application.db)
-			.all()
-			.mapEach { (route) in
-				route.delete(on: context.application.db)
-			}
-			.whenSuccess { (_) in
-				_ = Route(from: gpx.routes.first!).save(on: context.application.db)
-			}
-		Stop.query(on: context.application.db)
-			.all()
-			.mapEach { (stop) in
-				stop.delete(on: context.application.db)
-			}
-			.whenSuccess { (_) in
-				gpx.waypoints
-					.map { (gpxWaypoint) in
-						return Stop(from: gpxWaypoint)!
-					}
-					.forEach { (stop) in
-						_ = stop.save(on: context.application.db)
-					}
-			}
+//		let bundle = Bundle(path: "/app/.swift-bin/Shuttle Tracker Server_App.resources")!
+//		let routeFileURL = bundle.url(forResource: "Route", withExtension: "gpx")!
+//		let gpx = GPXParser(withURL: routeFileURL)!.parsedData()!
+//		Route.query(on: context.application.db)
+//			.all()
+//			.mapEach { (route) in
+//				route.delete(on: context.application.db)
+//			}
+//			.whenSuccess { (_) in
+//				_ = Route(from: gpx.routes.first!).save(on: context.application.db)
+//			}
+//		Stop.query(on: context.application.db)
+//			.all()
+//			.mapEach { (stop) in
+//				stop.delete(on: context.application.db)
+//			}
+//			.whenSuccess { (_) in
+//				gpx.waypoints
+//					.map { (gpxWaypoint) in
+//						return Stop(from: gpxWaypoint)!
+//					}
+//					.forEach { (stop) in
+//						_ = stop.save(on: context.application.db)
+//					}
+//			}
 		return context.eventLoop.future()
 	}
 	
