49b4431130fd008adeabde5f3a89ef34d21aec13
SudanySwag <homdykalefa@gmail.com>
2022-12-09 16:00:20 -0500

    Working version of monthly routine

--- Sources/App/Jobs/MonthlyReset.swift
+++ Sources/App/Jobs/MonthlyReset.swift
@@ -17,11 +17,16 @@ struct MonthlyReset: AsyncScheduledJob {
 	
 	func run(context: QueueContext) async throws {
 		let analyticsentries = try await AnalyticsEntry
-			.query(on: context.application.db)
+			.query(on: context.application.db(.psql))
 			.all()
+		let calendar = Calendar.current
 		for analyticsentry in analyticsentries {
+			if (calendar.dateComponents([.month], from: analyticsentry.dateSent, to: Date()).month! >= 6){
+				try await analyticsentry.delete(on: context.application.db(.psql))
+				continue
+			}
             analyticsentry.timesBoarded = 0
-			try await analyticsentry.update(on: context.application.db)
+			try await analyticsentry.update(on: context.application.db(.psql))
 		}
 	}
 	
