988a46f9e6b81e768542b643756a1e0cee6ddaa2
Gabriel Jacoby-Cooper <gjacobycooper@icloud.com>
2023-09-19 12:31:38 -0400

    Improve comments in LocationRemovalJob.swift

--- Sources/App/Jobs/LocationRemovalJob.swift
+++ Sources/App/Jobs/LocationRemovalJob.swift
@@ -14,7 +14,7 @@ struct LocationRemovalJob: AsyncScheduledJob {
 		let buses = try await Bus
 			.query(on: context.application.db(.sqlite))
 			.all()
-		let routes = try? await Route // Failing to query route objects shouldn’t cause this method to fail entirely
+		let routes = try? await Route // Failing to query route objects shouldn’t cause this method to fail entirely.
 			.query(on: context.application.db(.sqlite))
 			.all()
 			.filter { (route) in
@@ -23,16 +23,17 @@ struct LocationRemovalJob: AsyncScheduledJob {
 		for bus in buses {
 			bus.locations
 				.filter { (location) in
-					return location.type == .user && location.date.timeIntervalSinceNow < -30 // The time interval since now will be negative since the location’s timestamp will be in the past
+					return location.type == .user && location.date.timeIntervalSinceNow < -30 // The time interval since now will be negative since the location’s timestamp will be in the past.
 				}
 				.compactMap { (location) in
 					return bus.locations.firstIndex(of: location)
 				}
 				.forEach { (index) in
-					bus.locations.remove(at: index) // It’s safe to remove locations here because we’re iterating over a filtered, mapped copy of the original array, not the original array itself
+					bus.locations.remove(at: index) // It’s safe to remove locations here because we’re iterating over a filtered, mapped copy of the original array, not the original array itself.
 				}
 			if let routes {
-				bus.detectRoute(selectingFrom: routes) // Detect the most recent route association, resetting it to nil if there’s no sufficiently recent location data
+				// Detect the most recent route association, resetting it to nil if there’s no sufficiently recent location data
+				bus.detectRoute(selectingFrom: routes)
 			}
 			try await bus.update(on: context.application.db(.sqlite))
 		}
