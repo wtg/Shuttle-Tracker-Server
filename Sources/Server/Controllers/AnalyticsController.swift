c0a17d02daca19b81f1d35d3d2160950e93b8a82
chi <chijiokeokose@gmail.com>
2024-04-12 16:39:07 -0400

    added inline documentation to analyticsController.swift

--- Sources/Server/Controllers/AnalyticsController.swift
+++ Sources/Server/Controllers/AnalyticsController.swift
@@ -13,17 +13,24 @@ import Vapor
 struct AnalyticsController<DecoderType>: RouteCollection where DecoderType: ContentDecoder {
 	
 	private let decoder: DecoderType
+	/// Initialize a new analytics controller with the specified decoder.
+    /// - Parameter decoder: An object that conforms to the `ContentDecoder` protocol.
 	
 	init(decoder: DecoderType) {
 		self.decoder = decoder
 	}
 	
+    /// Registers all the routes related to analytics.
+    /// - Parameter routes: A builder object for registering routes.
 	func boot(routes: any RoutesBuilder) throws {
 		routes.get("userids", use: self.userIDs(_:))
 		routes.get("boardbus", "average", use: self.boardBusAverage(_:))
 		try routes.register(collection: AnalyticsEntriesController(decoder: self.decoder), on: "entries")
 	}
 	
+	/// Retrieves a list of unique user IDs from analytics entries.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: An array of `UUID` representing unique user IDs.
 	private func userIDs(_ request: Request) async throws -> [UUID] {
 		return try await AnalyticsEntry
 			.query(on: request.db(.psql))
@@ -32,6 +39,9 @@ struct AnalyticsController<DecoderType>: RouteCollection where DecoderType: Cont
 			.compactMap { return $0 } // Remove nil elements
 	}
 	
+	/// Calculates the average number of board bus counts per user.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: The average number of times users have boarded the bus.
 	private func boardBusAverage(_ request: Request) async throws -> Double {
 		let chunks = try await AnalyticsEntry
 			.query(on: request.db(.psql))
