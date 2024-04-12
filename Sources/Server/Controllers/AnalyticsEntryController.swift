f4308192855064fe9cf96353e3a360b52204b9d4
chi <chijiokeokose@gmail.com>
2024-04-12 16:50:02 -0400

    added inline documentation to analyticsEntryController.swift

--- Sources/Server/Controllers/AnalyticsEntryController.swift
+++ Sources/Server/Controllers/AnalyticsEntryController.swift
@@ -10,12 +10,19 @@ import Vapor
 /// A structure that registers routes for managing individual analytics entries.
 /// - Remark: In the context of this structure, the term “route” refers to an HTTP route, not a shuttle route.
 struct AnalyticsEntryController: RouteCollection {
-	
+
+	/// Registers a route for reading an individual analytics entry.
+    /// - Parameter routes: A builder object for registering routes. Routes are grouped by entry ID.
 	func boot(routes: any RoutesBuilder) throws {
 		routes.group(":id") { (routes) in
 			routes.get(use: self.read(_:))
 		}
 	}
+
+	/// Retrieves an analytics entry by its ID.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request, including the entry ID.
+    /// - Returns: The requested `AnalyticsEntry` object.
+    /// - Throws: An `Abort` error if the entry is not found, returning a 404 Not Found status.
 	
 	private func read(_ request: Request) async throws -> AnalyticsEntry {
 		let entry = try await AnalyticsEntry.find(
