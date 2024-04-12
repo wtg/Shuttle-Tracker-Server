2d87b1e14deb0ed08181d2305d6806b20e8293f3
chi <chijiokeokose@gmail.com>
2024-04-12 17:00:01 -0400

    added inline documentation to AnnouncementController.swift

--- Sources/Server/Controllers/AnnouncementController.swift
+++ Sources/Server/Controllers/AnnouncementController.swift
@@ -14,17 +14,24 @@ struct AnnouncementController<DecoderType>: RouteCollection where DecoderType: C
 	
 	private let decoder: DecoderType
 	
+	/// Initializes a new announcement controller with the specified decoder.
+    /// - Parameter decoder: An object that conforms to the `ContentDecoder` protocol, used for decoding the content of incoming requests.
 	init(decoder: DecoderType) {
 		self.decoder = decoder
 	}
 	
+	/// Registers routes for accessing and deleting announcements by their ID.
+    /// - Parameter routes: A builder object for registering routes.
 	func boot(routes: any RoutesBuilder) throws {
 		routes.group(":id") { (routes) in
 			routes.get(use: self.read(_:))
 			routes.delete(use: self.delete(_:))
 		}
 	}
-	
+	/// Retrieves an announcement by its ID.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request, including the announcement ID.
+    /// - Returns: The `Announcement` object requested.
+    /// - Throws: An `Abort` error if the announcement is not found (404) or if the ID is not provided (400).
 	private func read(_ request: Request) async throws -> Announcement {
 		guard let id = request.parameters.get("id", as: UUID.self) else {
 			throw Abort(.badRequest)
@@ -38,7 +45,11 @@ struct AnnouncementController<DecoderType>: RouteCollection where DecoderType: C
 		}
 		return announcement
 	}
-	
+
+	/// Deletes an announcement by its ID after verifying the provided digital signature.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request, including the announcement ID and a deletion request with a digital signature.
+    /// - Returns: The UUID of the deleted announcement.
+    /// - Throws: An `Abort` error if the ID is not provided (400), if signature verification fails (403), or if there are server issues during verification (500).
 	private func delete(_ request: Request) async throws -> UUID {
 		guard let id = request.parameters.get("id", as: UUID.self) else {
 			throw Abort(.badRequest)
