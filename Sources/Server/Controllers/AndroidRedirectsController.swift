0e0942d8506d4157bb1c98436458b24f8660c03d
chi <chijiokeokose@gmail.com>
2024-04-12 16:55:42 -0400

    added inline documentation to AndroidRedirectsController.swift

--- Sources/Server/Controllers/AndroidRedirectsController.swift
+++ Sources/Server/Controllers/AndroidRedirectsController.swift
@@ -12,15 +12,23 @@ import Vapor
 /// - Remark: In the context of this structure, the term “route” refers to an HTTP route, not a shuttle route.
 struct AndroidRedirectsController: RouteCollection {
 	
+	/// Registers routes for redirecting to the main and beta versions of the Android app.
+    /// - Parameter routes: A builder object for registering routes
 	func boot(routes: any RoutesBuilder) throws {
 		routes.get(use: self.index(_:))
 		routes.get("beta", use: self.beta(_:))
 	}
 	
+	/// Redirects to the main Android app page on Google Play.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: A `Response` object that performs a redirect to the app's page.
 	private func index(_ request: Request) -> Response {
 		return request.redirect(to: "https://play.google.com/store/apps/details?id=edu.rpi.shuttletracker")
 	}
 	
+	/// Redirects to the beta version of the Android app page on Google Play.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: A `Response` object that performs a redirect to the beta app's page.
 	private func beta(_ request: Request) -> Response {
 		return request.redirect(to: "https://play.google.com/store/apps/details?id=edu.rpi.shuttletracker")
 	}
