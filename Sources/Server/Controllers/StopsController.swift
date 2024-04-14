31fcafb7aac35f2d86ce699e8e098b25625154f8
chi <chijiokeokose@gmail.com>
2024-04-14 09:51:19 -0400

    added inline documentation to StopsController.swift

--- Sources/Server/Controllers/StopsController.swift
+++ Sources/Server/Controllers/StopsController.swift
@@ -12,11 +12,18 @@ import Vapor
 /// - Remark: In the context of this structure, the term “route” refers to an HTTP route, not a shuttle route.
 struct StopsController: RouteCollection {
 	
+	/// Registers HTTP routes for retrieving shuttle stop data and manages individual stop details through a nested controller.
+    /// - Parameter routes: A builder object for registering routes.
+    /// - Throws: Throws an error if the routes cannot be registered properly.
 	func boot(routes: any RoutesBuilder) throws {
 		routes.get(use: self.read(_:))
 		try routes.register(collection: StopController())
 	}
 	
+	/// Retrieves all active shuttle stops from the database, ensuring each stop is returned only once.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: An array of `Stop` objects representing active shuttle stops.
+    /// - Throws: Throws an error if there is a failure in fetching the data from the database.
 	private func read(_ request: Request) async throws -> [Stop] {
 		let stops = try await Stop
 			.query(on: request.db(.sqlite))
