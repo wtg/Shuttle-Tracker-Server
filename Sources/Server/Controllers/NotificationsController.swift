7ec6f6ee3b02b3aed16f6fc4f3c52fdbdf29b3fb
chi <chijiokeokose@gmail.com>
2024-04-14 09:40:29 -0400

    added inline documentation to NotificationsController.swift

--- Sources/Server/Controllers/NotificationsController.swift
+++ Sources/Server/Controllers/NotificationsController.swift
@@ -10,7 +10,11 @@ import Vapor
 /// A structure that registers routes for notifications.
 /// - Remark: In the context of this structure, the term “route” refers to an HTTP route, not a shuttle route.
 struct NotificationsController: RouteCollection {
-	
+
+
+	/// Registers routes for managing notification devices within the notifications framework.
+    /// - Parameter routes: A builder object for registering routes.
+    /// - Throws: Throws an error if the routes cannot be registered properly.
 	func boot(routes: RoutesBuilder) throws {
 		try routes.register(collection: NotificationsDevicesController(), on: "devices")
 	}
