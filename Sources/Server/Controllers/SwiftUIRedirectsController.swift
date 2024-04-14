93cdd32e31de72ca6ca5cd4013b474df71d12570
chi <chijiokeokose@gmail.com>
2024-04-14 09:53:00 -0400

    added inline documentation to SwiftUIReadirectsController.swift

--- Sources/Server/Controllers/SwiftUIRedirectsController.swift
+++ Sources/Server/Controllers/SwiftUIRedirectsController.swift
@@ -12,15 +12,25 @@ import Vapor
 /// - Remark: In the context of this structure, the term “route” refers to an HTTP route, not a shuttle route.
 struct SwiftUIRedirectsController: RouteCollection {
 	
+	/// Registers HTTP routes for redirecting users to the main and beta versions of the SwiftUI app.
+    /// - Parameter routes: A builder object for registering routes.
+    /// - Throws: Throws an error if the routes cannot be registered properly.
+    
 	func boot(routes: any RoutesBuilder) throws {
 		routes.get(use: self.index(_:))
 		routes.get("beta", use: self.beta(_:))
 	}
 	
+	/// Redirects to the main SwiftUI app page on the App Store.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: A `Response` object that performs a redirect to the app's page on the App Store.
 	private func index(_ request: Request) -> Response {
 		return request.redirect(to: "https://apps.apple.com/us/app/shuttle-tracker/id1583503452")
 	}
 	
+	/// Redirects to the beta version of the SwiftUI app on TestFlight.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: A `Response` object that performs a redirect to the beta app's page on TestFlight.
 	private func beta(_ request: Request) -> Response {
 		return request.redirect(to: "https://testflight.apple.com/join/GsmZkfgd")
 	}
