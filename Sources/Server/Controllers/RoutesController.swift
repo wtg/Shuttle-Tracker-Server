5730e0ba76dad13554e9ee3c2f170ffa438d865d
chi <chijiokeokose@gmail.com>
2024-04-14 09:47:06 -0400

    added inline documentation to RoutesController.swift

--- Sources/Server/Controllers/RoutesController.swift
+++ Sources/Server/Controllers/RoutesController.swift
@@ -12,11 +12,18 @@ import Vapor
 /// - Remark: In the context of this structure, the term “route” could refer to either an HTTP route or a shuttle route, depending on the local context.
 struct RoutesController: RouteCollection {
 	
+	/// Registers an HTTP route that is used to read shuttle route data.
+    /// - Parameter routes: A builder object for registering routes.
+    /// - Throws: Throws an error if the route cannot be registered properly.
 	func boot(routes: any RoutesBuilder) throws {
 		// In the context of this method, the term “route” refers to an HTTP route, not a shuttle route.
 		routes.get(use: self.read(_:))
 	}
 	
+	/// Retrieves all active shuttle routes from the database.
+    /// - Parameter request: A `Request` object encapsulating details about the incoming request.
+    /// - Returns: An array of `Route` objects representing the active shuttle routes.
+    /// - Throws: Throws an error if there is a failure in fetching the data from the database.
 	private func read(_ request: Request) async throws -> [Route] {
 		// In the context of this method, the term “route” refers to a shuttle route, not an HTTP route.
 		return try await Route
